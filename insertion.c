240902ff45f99fc65d42e805db6d1b434732ab12
cesarsapha <162627241+cesarsapha@users.noreply.github.com>
2024-12-11 20:07:37 -0300

    insertion.c

--- insertion.c
+++ insertion.c
@@ -2,34 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TAM 400000
-
 int main(){
-    int i, j, x, v[TAM];
+    int i, j, x, v[400000];
     clock_t t;
 
-    //aleatoriedade
     srand((unsigned)time(NULL));
 
-    //adiciona valores no vetor
-    for (int i = 0; i < TAM; i++){
-        v[i] = rand()%TAM;
+    for (int i = 0; i < 400000; i++){
+        v[i] = rand()%400000;
     }
     
     t = clock();
-    //ordena o vetor por meio do insertionSort
-    for (i = 1; i < TAM; i++){
+    for (i = 1; i < 400000; i++){
         x = v[i];
-
         for (j = i-1; j >= 0 && v[j] > x; j--){
             v[j+1] = v[j];
         }
 
     v[j+1] = x;
-        
     }
-
     t = clock() - t;
-
     printf("Tempo de execucao: %.6lf ms\n", ((double)t)/((CLOCKS_PER_SEC/1000)));
 }
