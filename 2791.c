c3e9fa6ba048dd7a34de24bf5323609f6dd24560
cesarsapha <162627241+cesarsapha@users.noreply.github.com>
2025-02-12 11:52:51 -0300

    Add files via upload

---
+++ 2791.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int C1, C2, C3, C4;
+    scanf("%d %d %d %d", &C1, &C2, &C3, &C4);
+
+    int copo = (C1*1+C2*2+C3*3+C4*4);
+
+    printf("%d\n", copo);
+
+    return 0;
+}
